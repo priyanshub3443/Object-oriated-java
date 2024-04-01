70f70708fe970d4ace3e5735def719e64944122f
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-04-01 16:30:34 +0530

    pritice set

---
+++ gpa.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class gpa {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        
+        // float e = 45f;
+        // float h = 67f;
+        // float m = 98f;
+        // float gpa = (e + h + m) / 30;
+        // System.out.println(gpa);
+        // Qustion 2
+        // Scanner scanner= new Scanner(System.in);
+        // String name= scanner.nextLine();
+        // System.out.println("hello " + name + " have a good day");
+        // Qustion 3
+        double kilometers = 10; // Change this value to the number of kilometers you want to convert
+        double miles = kilometers * 0.621371;
+        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+    }
+
+}
