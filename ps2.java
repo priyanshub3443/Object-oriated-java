8db14230bdfca9a839a1a30a043bbee6f6d32a28
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-04-01 16:43:53 +0530

    Update ps2.java

---
+++ ps2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ps2 {
+    public static void main(String[] args) {
+        // Qustion 1;
+        // what will be the resul of the float a= 7/4*9/2
+        // float a= 7/4*9/2;
+        // System.out.println(a);
+
+        // qustion 2
+        // wrie a java program to encry[t a grade by adding 87 to it. decrypt it to show the corect grade]
+        // char grade='b';
+        // grade=(char)(grade+8);
+        // System.out.println(grade);
+        
+        //qustion 3
+        // use comparison operators to find out wheter a give number is greater than the user entere number or not 
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a>8);
+        System.out.println(7*49/7+35/7);
+       
+    }    
+}
