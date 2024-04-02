86b5ad54633d393ecc09a60df729e1bdf18814f0
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-04-02 16:45:30 +0530

    change

---
+++ ps4.java
@@ -0,0 +1,16 @@
+public class ps4 {
+    public static void main(String[] args) {
+        // qustion 1
+        // int a= 10;
+        // if (a==11) {
+        //     System.out.println("I am 11");
+        // }
+        // else{
+        //     System.out.println("I am not 11");
+        // }
+        
+        // qustion 2
+      //  write fa progrma to find out wheter a stidemt os pass pr faol]
+      int sub1 , sub2, sub3;
+    }
+}
