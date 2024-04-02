bf9234f6d98c49082e21f2a89a55189261b6e1d1
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2024-04-02 16:35:54 +0530

    Create ps3.java

---
+++ ps3.java
@@ -0,0 +1,25 @@
+public class ps3 {
+    public static void main(String[] args) {
+        // String s="priyanshu";
+        // System.out.println(s.indexOf("yan"));
+        //write a java program to convert a string to lower case
+    //     String s= "PRIYANSHU";
+    //   var name=s.toLowerCase();
+    //     System.out.println(name);
+    // }
+    
+    // qustion 2
+    // write a java program to replace spaces with underscores
+    // String s= "this is a very     good boy";
+    // System.out.println(s.replace("   ", " --- "));
+     // qustion 3
+     // write a java progrma to fill in a letter a leeter template which books like below 
+    //  String letter="Dear <|name|>, tnas a bt ";
+    //  System.out.println(letter.replace("<|name|>", "priyanshu"));
+     // qustion 4
+     // write a java program to detect double and tipe space in a string
+     String myString = "This string contains double and  triple spaces";
+     System.out.println(myString.indexOf("  "));
+     System.out.println(myString.indexOf("   "));
+}
+}
